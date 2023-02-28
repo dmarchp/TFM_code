709ea1a50b5695782977e05111aef418a15054aa
dmarchp <dmarchp@gmail.com>
2023-02-28 17:39:57 +0100

    work 28 feb

--- sim_some_params_frozen/percolation.py
+++ sim_some_params_frozen/percolation.py
@@ -1,9 +1,15 @@
 from graph_properties_functions import *
 import matplotlib.pyplot as plt
 from math import pi
+from scipy.special import factorial
+import numpy as np
+import pandas as pd
 from scipy.optimize import curve_fit
 from numpy import linspace
 from collections import Counter
+import sys
+sys.path.append('..')
+from global_functions import *
 
 exclusion_r = 1.5
 #arena_r = 20.0
@@ -127,6 +133,38 @@ def plotComSizes_difN(Ns, arena_rs, interac_rs, exclusion_r, prob=False, push=Fa
     figName += f'_er_{exclusion_r}_{pushLabel}_{gcLabel}.png'
     fig.savefig(figName)
 
+# this should probably go to graph_properties-functiions
+def getDegreeDistr(N, arena_r, interac_r, exclusion_r, push=False, toFile=True):
+    pushLabel = 'push' if push else 'nopush'
+    filename = f'other_res_files/degreeDistr_N_{N}_ar_{arena_r}_ir_{interac_r}_er_{exclusion_r}_{pushLabel}.csv'
+    degrees = getBotDegrees(N, exclusion_r, interac_r, arena_r, push=push)
+    binCenters = np.linspace(min(degrees), max(degrees), max(degrees)-min(degrees)+1)
+    binLims = np.linspace(min(degrees), max(degrees)+1, max(degrees)+1-min(degrees)+1) - 0.5
+    binCenters, prob, dprob = hist1D(degrees, binLims, binCenters, isPDF = True)
+    df = pd.DataFrame({'binCenters':binCenters, 'prob':prob, 'dprob':dprob})
+    if toFile:
+        df.to_csv(filename, index=False)
+    return df
+    
+def plotDegreeDistr_manyir(N, arena_r, irs, exclusion_r, push=False, poisson=False):
+    fig, ax = plt.subplots()
+    for ir in irs:
+        df = getDegreeDistr(N, arena_r, ir, exclusion_r, push=push)
+        line, = ax.plot(df['binCenters'], df['prob'], label=f'$r_i = {ir}$', ls=':', lw=0.7, marker='.')
+        if poisson:
+            degrees = getBotDegrees(N, exclusion_r, ir, arena_r, push=push)
+            avgdeg = np.mean(degrees)
+            expo = np.exp(-avgdeg)
+            df['poisson'] = expo*(avgdeg**df['binCenters'])/(factorial(df['binCenters']))
+            ax.plot(df['binCenters'], df['poisson'], lw=0.8, color = line.get_color(), alpha=0.5)
+    ax.set(xlabel='Degree, $k$', ylabel='$P(k)$')
+    fig.legend(loc=(0.7, 0.6), fontsize=9)
+    fig.suptitle(f'N = {N}, $r_a = {arena_r}$, Quenched', fontsize=9)
+    fig.tight_layout()
+    fig.savefig(f'degreeDistr_N_{N}_ar_{arena_r}_er_{exclusion_r}_quenched.png')
+
+
+
 
 def main():
     ###### mcs individual sizes ######
@@ -146,8 +184,11 @@ def main():
     #componentsHistogram(492, 6.0, 1.5, 75.0)
     ####
     # a dia 21 de febrer 2023 ho estic fent amb 1000 configs N35, 250 configs N492
-    plotComSizes_difN([35,492], [20.0, 75.0], [6.5, 6.4], 1.5, prob=True, push=False, excludeGiantComp = False, dataToFile=True)
-    plotComSizes_difN([35,492], [20.0, 75.0], [6.5, 6.4], 1.5, prob=True, push=False, excludeGiantComp = True, dataToFile=True)
+    # plotComSizes_difN([35,492], [20.0, 75.0], [6.5, 6.4], 1.5, prob=True, push=False, excludeGiantComp = False, dataToFile=True)
+    # plotComSizes_difN([35,492], [20.0, 75.0], [6.5, 6.4], 1.5, prob=True, push=False, excludeGiantComp = True, dataToFile=True)
+    plotDegreeDistr_manyir(35, 20.0, [5.0, 6.5, 8.0, 9.0], 1.5, poisson=True)
+    plotDegreeDistr_manyir(492, 75.0, [4.9, 6.4, 7.6, 9.1], 1.5, poisson=True)
+
     
 
 if __name__ == '__main__':
