038501cc2410f11fed5d5e26fa92fa50029cf1bf
David <dmarchp@gmail.com>
2024-04-29 16:24:07 +0200

    work 29 apr

--- gillespie_sim_ci/LES_model_gill.py
+++ gillespie_sim_ci/LES_model_gill.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import argparse
 import copy
 import sys
+import os
 from subprocess import call
 sys.path.append('../')
 from package_global_functions import *
@@ -13,6 +14,15 @@ import matplotlib.pyplot as plt
 
 # sample exec command if u feel lazy
 # python LES_model_gill.py -pis 0.1,0.1 -qs 9.0,10.0 -l 0.8 -lci 0.9 -N 500 -maxTime 150.0  -Nrea 10 -ic p10-0-90 -time_evo True -time_evo_plot True -ci_kwargs 1,0.35,40
+
+extSSDpath = getExternalSSDpath()
+if os.path.exists(extSSDpath):
+    resPath = extSSDpath + getProjectFoldername() + '/gillespie_sim_ci/results'
+else:
+    resPath = '/results'
+
+if not os.path.exists(resPath):
+    call(f'mkdir -p {resPath}', shell=True)
 
 
 # def cross_in_func(pop,*kwargs):
@@ -113,6 +123,7 @@ def LESgillespieSim(initial_state, save_time_evo=False, avg_last_perc=0.2):
         time_evo = [[0.0],]
         aux = [[state[i]/N, ] for i in range(Nsites+1)]
         time_evo.extend(aux)
+    ######################## START SIMULATION LOOP ########################
     state_ss_avg = [[], [], []]
     while t < maxTime:
         # prevState = copy.deepcopy(state)
@@ -129,8 +140,15 @@ def LESgillespieSim(initial_state, save_time_evo=False, avg_last_perc=0.2):
                 state_ss_avg[i].append(state[i])
         if simFinished:
             break
+    ######################## FINISH SIMULATION LOOP ########################
+    # before converting values to average, save them for the SS distribution, if required:
+    if saveSSdata:
+        for i in range(Nsites+1):
+            ssDataPool[i].extend(state_ss_avg[i][-2000:]) # only use the last 2000 values, so every realization contributes the same
+    # now get the average values for this realization
     for i in range(Nsites+1):
         state_ss_avg[i] = np.average(state_ss_avg[i])
+    # return final state, final SS average, and time evolution (if required)
     if save_time_evo:
         dfevo = pd.DataFrame({'time':time_evo[0]})
         for i in range(1,Nsites+2):
@@ -151,12 +169,19 @@ if __name__ == '__main__':
     parser.add_argument('-maxTime', type=float, help='simulation time')
     parser.add_argument('-Nrea', type=int, help='Number of realizations')
     parser.add_argument('-ic', type=str, help="Initial conditions. N for all uncomitted; E for equipartition bt sites; E for equipartition bt sites and uncomitted;")
-    parser.add_argument('-time_evo', type=bool, help='True: save time evos', default=False)
-    parser.add_argument('-time_evo_plot', type=bool, help='True: plot time evos; -time_evo must also be true', default=False)
-    parser.add_argument('-final_state', type=bool, help="True: print each rea's final state", default=True)
+    # boolean arguments 
+    # parser.add_argument('-time_evo', type=bool, help='True: save time evos', default=False)
+    # parser.add_argument('-time_evo_plot', type=bool, help='True: plot time evos; -time_evo must also be true', default=False)
+    # parser.add_argument('-final_state', type=bool, help="True: print each rea's final state", default=True)
+    # parser.add_argument('-ss_data', type=bool, help='Save SS values in a dataframe', default=False)
+    # redesign:
+    parser.add_argument('--final_state', type=bool, help='Print each realization final state', action=argparse.BooleanOptionalAction)
+    parser.add_argument('--ss_data', type=bool, help='Save SS values in a dataframe', action=argparse.BooleanOptionalAction)
+    parser.add_argument('--time_evo', type=bool, help='Save time evolutions as df', action=argparse.BooleanOptionalAction)
+    parser.add_argument('--time_evo_plot', type=bool, help='Plot time evolutions', action=argparse.BooleanOptionalAction)
     args = parser.parse_args()
-    pis, qs, l, lci, ci_kwargs, N, maxTime, Nrea, ic, saveTimeEvo = args.pis, args.qs, args.l, args.lci, args.ci_kwargs, args.N, args.maxTime, args.Nrea, args.ic, args.time_evo
-    printFinalState = args.final_state
+    pis, qs, l, lci, ci_kwargs, N, maxTime, Nrea, ic = args.pis, args.qs, args.l, args.lci, args.ci_kwargs, args.N, args.maxTime, args.Nrea, args.ic
+    saveTimeEvo, printFinalState, saveSSdata = args.time_evo, args.final_state, args.ss_data
     # ci_kwargs = ['sigmoid1', 0.5, 50 ]
     # ci_kwargs = ['lin', ]
     ci_kwargs[0] = int(ci_kwargs[0])
@@ -178,6 +203,13 @@ if __name__ == '__main__':
     if saveTimeEvo:
         evosFolder = 'sim_results_evos'
         call(f'mkdir -p {evosFolder}/', shell=True)
+    if saveSSdata:
+        pichain = '_'.join([str(pi) for pi in pis])
+        qchain = '_'.join([str(q) for q in qs])
+        ci_kwargs_chain = '_'.join([str(cikw) for cikw in ci_kwargs])
+        ssData_fname = f'sim_pis_{pichain}_qs_{qchain}_l_{l}_lci_{lci}_cikw_{ci_kwargs_chain}_N_{N}_ic_{ic}.csv'
+        ssDataPool = [[] for i in range(Nsites+1)]
+    ##### START REALIZATIONS LOOP #####
     for i in range(Nrea):
         if saveTimeEvo:
             finalState, ssAvgState, dfevo = LESgillespieSim(bots_per_site, save_time_evo=True)
@@ -205,6 +237,18 @@ if __name__ == '__main__':
             for j in range(1,Nsites+1):
                 ax.plot(dfevo['time'], dfevo[f'f{j}'], color=colors[j])
             fig.savefig(f'time_evo_rea_{i}.png')
+    ##### END REALIZATIONS LOOP #####
+
+    ### save stationary state distribution:
+    if saveSSdata:
+        ssDF = {}
+        ssDataPool = np.array(ssDataPool)/N
+        for i in range(Nsites+1):
+            ssDF[f'f{i}'] = ssDataPool[i]
+        ssDF = pd.DataFrame(ssDF)
+        ssDF.to_csv(resPath + '/' + ssData_fname, index=False)
+        # save it to external drive:
+
         
         #### compute averages with last 20% of timesteps
         # timeForAvg = maxTime - 0.2*maxTime
